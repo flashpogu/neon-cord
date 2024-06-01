a7f6dd58b8b7698702f43c725d1b017c51d320c5
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-06-01 17:58:01 +0530

    implemented sockets chat components server channels and many more features

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,38 @@
+import { Channel, ChannelType, Server } from "@prisma/client";
+import { create } from "zustand";
+
+export type ModalType =
+  | "createServer"
+  | "invite"
+  | "editServer"
+  | "members"
+  | "createChannel"
+  | "leaveServer"
+  | "deleteServer"
+  | "deleteChannel"
+  | "editChannel"
+  | "messageFile";
+
+interface ModelData {
+  server?: Server;
+  channel?: Channel;
+  channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
+}
+
+interface ModalStore {
+  type: ModalType | null;
+  data: ModelData;
+  isOpen: boolean;
+  onOpen: (type: ModalType, data?: ModelData) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  data: {},
+  isOpen: false,
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ type: null, isOpen: false }),
+}));
